02cb1dc4141a66e96ea926e07d360672b3d86309
Rahul Gianchandani <74424338+RahulGianchandani@users.noreply.github.com>
2022-12-02 17:29:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import Fetch from "./Screens/Fetch"
+import Login from "./Screens/Login"
+import Register from "./Screens/Register"
+import Counter from "./Screens/Counter"
+import Paginate2 from "./Screens/Paginate2"
+import Pagination from "./Screens/Pagination"
+import ReducerHook from "./Screens/ReducerHook"
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PromiseAll from "./Screens/PromiseAll";
 
 function App() {
   return (
@@ -13,7 +22,13 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Fetch />} />
-      
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/pagination2" element={<Paginate2 />} />
+        <Route path="/pagination" element={<Pagination />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/Reducer" element={<ReducerHook />} />
+        <Route path="/promises" element={<PromiseAll />} />
       </Routes>
       <Footer />
       <ToastContainer />
